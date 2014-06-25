94f46e2476b639217bb72cc21adc427e43f7e038
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-25 11:56:06 -0700

    Added in build tasks for aggregating and minifying CSS and JS files.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,18 @@ var minify = require('gulp-minify-css');
 var jshint = require('gulp-jshint');
 var csslint = require('gulp-csslint');
 
+gulp.task('js-concat', function(){
+    return gulp.src('client/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('css-minify', function(){
+    return gulp.src(['client/css/**/*.css'])
+        .pipe(concat('app.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('build'));
+});
 
 gulp.task('node', function(){
   nodemon({
