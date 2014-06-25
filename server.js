116861dcae072b6b2d354e612cdd77f5cb960e9d
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-24 22:54:00 -0700

    Added in static servering of the webpages in /client.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static(__dirname + '/client'));
+app.use('/static', express.static(__dirname + '/build'));
+
 var server = app.listen(3000, function(){
   console.log('Listening on port 3000');
 });
