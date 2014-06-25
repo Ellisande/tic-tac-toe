e938157bd2894694a6903168ce59941a02858e0d
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-25 10:13:01 -0700

    Added js-concat and css-minify tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,19 @@ var minify = require('gulp-minify-css');
 var jshint = require('gulp-jshint');
 var csslint = require('gulp-csslint');
 
+gulp.task('js-concat', function(){
+    return gulp.src('client/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('css-minify', function(){
+    return gulp.src(['client/css/**/*.css'])
+        .pipe(concat('app.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('build'));
+});
+
 gulp.task('node', function(){
   nodemon({
       script: 'server.js',
@@ -16,4 +29,4 @@ gulp.task('node', function(){
     });
 });
 
-gulp.task('default', ['node']);
+gulp.task('default', ['node', 'js-concat', 'css-minify']);
