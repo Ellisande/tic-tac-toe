96d41d0c7a719dc57629bd566ba926c55f5aeaaa
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-25 14:51:17 -0700

    Added in a socket.on event to recieve the state of the game board. Also dynamically add the old board methods to the server object that is received.

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,31 +1,43 @@
 function HomeCtrl($scope, socket){
 
+  socket.on('board:update', function(data){
+    var board = data.board;
+    board.getRows = function(){
+      var row1 = [ this['1-1'], this['1-2'], this['1-3'] ];
+      var row2 = [ this['2-1'], this['2-2'], this['2-3'] ];
+      var row3 = [ this['3-1'], this['3-2'], this['3-3'] ];
+      return [row1, row2, row3];
+    };
+
+    board.getColumns = function(){
+      var column1 = [ this['1-1'], this['2-1'], this['3-1'] ];
+      var column2 = [ this['1-2'], this['2-2'], this['3-2'] ];
+      var column3 = [ this['1-3'], this['2-3'], this['3-3'] ];
+      return [column1, column2, column3];
+    };
+
+    board.getDiagonals = function(){
+      var diagonal1 = [ this['1-1'], this['2-2'], this['3-3'] ];
+      var diagonal2 = [ this['1-3'], this['2-2'], this['3-1'] ];
+      return [diagonal1, diagonal2];
+    };
+
+    board.changeMark = function(row, column){
+      var coordinates = row+'-'+column;
+      var marker = this[coordinates];
+      if(marker == 'X') this[coordinates] = 'O';
+      if(marker == 'O') this[coordinates] = '_';
+      if(marker == '_') this[coordinates] = 'X';
+    };
+
+    $scope.board = board;
+  });
   $scope.changeMarker = function(row, column){
-    board.changeMark(row, column);
+    $scope.board.changeMark(row, column);
   };
 
-  board.getRows = function(){
-    var row1 = [ this['1-1'], this['1-2'], this['1-3'] ];
-    var row2 = [ this['2-1'], this['2-2'], this['2-3'] ];
-    var row3 = [ this['3-1'], this['3-2'], this['3-3'] ];
-    return [row1, row2, row3];
-  }
-
-  board.getColumns = function(){
-    var column1 = [ this['1-1'], this['2-1'], this['3-1'] ];
-    var column2 = [ this['1-2'], this['2-2'], this['3-2'] ];
-    var column3 = [ this['1-3'], this['2-3'], this['3-3'] ];
-    return [column1, column2, column3];
-  }
-
-  board.getDiagonals = function(){
-    var diagonal1 = [ this['1-1'], this['2-2'], this['3-3'] ];
-    var diagonal2 = [ this['1-3'], this['2-2'], this['3-1'] ];
-    return [diagonal1, diagonal2];
-  }
 
   var getWinner = function(markerSet){
-    console.log(markerSet);
     if(markerSet[0] == markerSet[1] && markerSet[1] == markerSet[2] && markerSet[2] == 'X') {
       return 'X';
     }
@@ -37,33 +49,33 @@ function HomeCtrl($scope, socket){
 
 
   $scope.$watch('board', function(){
-    var rows = board.getRows();
+    var rows = $scope.board.getRows();
     var rowWinner = rows.some(function(row){
       var winner = getWinner(row);
       if(winner){
-        board.winner = winner;
+        $scope.board.winner = winner;
         return true;
       }
     });
 
-    var columns = board.getColumns();
+    var columns = $scope.board.getColumns();
     var columnWinner = columns.some(function(column){
       var winner = getWinner(column);
       if(winner){
-        board.winner = winner;
+        $scope.board.winner = winner;
         return true;
       }
     });
 
-    var diagonals = board.getDiagonals();
+    var diagonals = $scope.board.getDiagonals();
     var diagonalWinner = diagonals.some(function(diagonal){
       var winner = getWinner(diagonal);
       if(winner){
-        board.winner = winner;
+        $scope.board.winner = winner;
         return true;
       }
     });
 
-    if(!rowWinner && !columnWinner && !diagonalWinner) board.winner = null;
+    if(!rowWinner && !columnWinner && !diagonalWinner) $scope.board.winner = null;
   }, true);
 }
