e0e8a3efa3ed7ebf1c1ef31643ce154682a59564
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-25 14:34:39 -0700

    Moved the game board to the server side. Set the game board to start blank. Created an emit event to boardcast the board on client connection.

--- server.js
+++ server.js
@@ -8,8 +8,30 @@ var server = app.listen(3000, function(){
   console.log('Listening on port 3000');
 });
 
+var board = {
+  '1-1': '_',
+  '1-2': '_',
+  '1-3': '_',
+  '2-1': '_',
+  '2-2': '_',
+  '2-3': '_',
+  '3-1': '_',
+  '3-2': '_',
+  '3-3': '_',
+  winner: null,
+  changeMark: function(row, column){
+    var coordinate = row+'-'+column;
+    var currentMark = this[coordinate];
+    if(currentMark == 'X') this[coordinate] = 'O';
+    if(currentMark == 'O') this[coordinate] = '_';
+    if(currentMark == '_') this[coordinate] = 'X';
+  }
+};
+
 var io = require('socket.io')(server);
 
 io.on('connection', function(socket){
-  console.log('Client connected');
-})
+  socket.emit('board:update', {
+    board: board
+  });
+});
