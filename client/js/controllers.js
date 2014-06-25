70d1e3337d91645a180d13ae642fb0074ebb442f
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-25 12:28:11 -0700

    Added methods for getting rows, columns, and diagonals to the game board.

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -23,4 +23,26 @@ function HomeCtrl($scope, socket){
   $scope.changeMarker = function(row, column){
     board.changeMark(row, column);
   };
+
+  board.getRows = function(){
+    var row1 = [ this['1-1'], this['1-2'], this['1-3'] ];
+    var row2 = [ this['2-1'], this['2-2'], this['2-3'] ];
+    var row3 = [ this['3-1'], this['3-2'], this['3-3'] ];
+    return [row1, row2, row3];
+  }
+
+  board.getColumns = function(){
+    var column1 = [ this['1-1'], this['2-1'], this['3-1'] ];
+    var column2 = [ this['1-2'], this['2-2'], this['3-2'] ];
+    var column1 = [ this['1-3'], this['2-3'], this['3-3'] ];
+    return [column1, column2, column3];
+  }
+
+  board.getDiagonals = function(){
+    return diagonal1 = [ this['1-1'], this['2-2'], this['3-3'] ];
+    return diagonal2 = [ this['1-3'], this['2-2'], this['3-1'] ];
+    return [diagonal1, diagonal2];
+  }
+
+
 }
