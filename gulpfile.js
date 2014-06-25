5b9e0e7da935f91da39cf8db1536fe25114bf427
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-25 11:55:12 -0700

    Rolling back gulpfile to its original state.

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,6 @@ var minify = require('gulp-minify-css');
 var jshint = require('gulp-jshint');
 var csslint = require('gulp-csslint');
 
-gulp.task('js-concat', function(){
-    return gulp.src('client/js/**/*.js')
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('build'));
-});
-
-gulp.task('css-minify', function(){
-    return gulp.src(['client/css/**/*.css'])
-        .pipe(concat('app.css'))
-        .pipe(minify())
-        .pipe(gulp.dest('build'));
-});
-
-gulp.task('js-lint', function(){
-    return gulp.src('client/js/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-gulp.task('css-lint', function(){
-    return gulp.src('client/css/**/*.css')
-        .pipe(csslint())
-        .pipe(csslint.reporter('default'));
-});
-
-gulp.task('lint', ['js-lint', 'css-lint']);
-
-gulp.task('minify', ['css-minify','js-concat']);
 
 gulp.task('node', function(){
   nodemon({
@@ -45,9 +17,4 @@ gulp.task('node', function(){
     });
 });
 
-gulp.task('watch', function(){
-    gulp.watch('client/js/**/*.js', ['js-concat', 'js-lint']);
-    gulp.watch('client/css/**/*.css', ['css-minify', 'css-lint' ]);
-});
-
-gulp.task('default', ['node', 'lint', 'minify', 'watch']);
+gulp.task('default', ['node']);
