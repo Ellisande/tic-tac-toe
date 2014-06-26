bb1020671614fc7ba46c399b4914bead186f7d1f
Mayor McCheese <justin.dragos.m91s@statefarm.com>
2014-06-25 23:31:53 -0700

    Added a test for the winning functionality.

--- test/markChanges.js
+++ test/markChanges.js
@@ -11,5 +11,16 @@ module.exports = {
       .click('#grid-11')
       .assert.containsText('#grid-11', '_')
       .end();
+  },
+
+  "Changing an entire row to X should make it say that X is winning": function(browser){
+    browser
+      .url('http://localhost:3000')
+      .waitForElementVisible('body', 1000)
+      .click('#grid-11')
+      .click('#grid-12')
+      .click('#grid-13')
+      .assert.containsText('#winning', 'X')
+      .end();
   }
 };
